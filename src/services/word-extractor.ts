42494e349e6adb232990063a6736eb62fef9fd67
Bruno PACHECO <bpacheco@pictet-technologies.com>
2022-01-23 15:07:51 +0100

    Devide word meaning into types.

--- src/services/word-extractor.ts
+++ src/services/word-extractor.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from "inversify";
-import { Dictionary, Word } from "../model/word";
+import { Dictionary, Word, WordType, WordTypeDetails } from "../model/word";
 import * as fs from "fs";
 import * as path from "path";
 import { JSDOM } from "jsdom";
@@ -42,23 +42,37 @@ export class WordExtractorImpl implements WordExtractor {
 
                     const meanings = this.readMeanings(lodKey, lodTempFolder);
                     const wordFound = this.extractWord(meanings);
-                    const {type, gender, verbGroup} = this.extractWordTypeAndGenderAndVerbGroup(meanings);
+                    const wordFoundKey = this.keyGenerator.generateWordKey(wordFound);
+                    const wordTypes = this.extractWordTypes(meanings);
+
                     const wordObj: Word = {
-                        id: this.keyGenerator.generateWordKey(wordFound),
+                        id: wordFoundKey,
                         word: wordFound,
-                        type: type,
-                        wordDetails: {
-                            nounGender: gender
-                        },
-                        meanings: [],
+                        types: wordTypes 
                     };
-                    console.log(wordObj);
+
+                    if(extractedWords.has(wordFoundKey)) {
+                        const previousWordObj = extractedWords.get(wordFoundKey);
+                        for(const type of wordObj.types) {
+                            const previousOtherType = previousWordObj!.types.find(otherType => otherType.type === type.type);
+                            if(!!previousOtherType) {
+                                previousOtherType.meanings = [...previousOtherType.meanings, ...type.meanings];
+                            } else {
+                                previousWordObj!.types.push(type);
+                            }
+                        }
+                    } else {
+                        extractedWords.set(wordFoundKey, wordObj);
+                    }
+
                     console.log(`${lodKey} was extracted.`);
                 }
             } catch(exception) {
                 console.error(exception);
             }
         }
+
+        extractedWords.forEach(word => console.log(JSON.stringify(word)));
     }
 
     private cannotExtractWord(lodKey: string, wordKey: string, lodTempFolder: string, wordTempFolder: string): boolean {
@@ -81,11 +95,7 @@ export class WordExtractorImpl implements WordExtractor {
         return element.textContent.trim();
     }
 
-    private extractWordTypeAndGenderAndVerbGroup(dom: JSDOM): {
-        type: string;
-        gender?: string;
-        verbGroup?: string;
-    } {
+    private extractWordTypes(dom: JSDOM): WordType[] {
         const element = dom.window.document.querySelector("div#article > span:nth-child(2)");
         if(!element || !element.textContent) {
             throw new Error("Word Type not found.");
@@ -94,34 +104,67 @@ export class WordExtractorImpl implements WordExtractor {
 
         let notTranslatedType = rawType;
         let notTranslatedGender: string | null = null;
-        let verbGroup: string | undefined = undefined;
         if(rawType.includes(" ")) {
             [notTranslatedGender, notTranslatedType] = rawType.split(" ");
         }
 
-        let type: string;
+        let typeKeys: string[];
         console.log(notTranslatedType);
         switch(notTranslatedType) {
-            case "adjektiv": type = "adjective"; break;
-            case "adverb": type = "adverb"; break;
-            case "hëllefsverb": type = "verb"; verbGroup = "auxiliary"; break;
-            case "konjunktioun": type = "conjunction"; break;
-            case "modalverb": type = "modal verb"; break;
-            case "substantiv": type = "noun"; break;
-            case "verb": type = "verb"; break;
+            case "adjektiv": typeKeys = ["adjective"]; break;
+            case "partikel": typeKeys = ["particle"]; break;
+            case "adverb": typeKeys = ["adverb"]; break;
+            case "konjunktioun": typeKeys = ["conjunction"]; break;
+            case "substantiv": typeKeys = ["noun"]; break;
+            case "verbpartikel": typeKeys = ["verb particle"]; break;
+            case "hëllefsverb": typeKeys = ["auxiliary verb"]; break;
+            case "modalverb": typeKeys = ["modal verb"]; break;
+            case "verb": typeKeys = ["transitive verb", "reflexive verb", "intransitive verb", "transitive impersonal verb"]; break;
             default: throw new Error("Word Type not found.");
         }
 
-        if(!notTranslatedGender) {
-            return {type: type, gender: undefined, verbGroup: verbGroup};
+        let auxiliaryVerb: string | undefined = undefined;
+        let pastParticiple: string | undefined = undefined;
+        if(["hëllefsverb", "modalverb", "verb"].includes(notTranslatedType)) {
+            const auxiliaryVerbEl = dom.window.document.querySelector("div#article > span:nth-child(3)");
+            if(!auxiliaryVerbEl || !auxiliaryVerbEl.textContent) {
+                throw new Error("Auxiliary Verb not found.");
+            }
+            auxiliaryVerb = auxiliaryVerbEl.textContent.trim().split(" ")[1];
+            const pastParticipleEl = dom.window.document.querySelector("div#article > span:nth-child(6)");
+            if(!pastParticipleEl || !pastParticipleEl.textContent) {
+                throw new Error("Past Participle not found.");
+            }
+            pastParticiple = pastParticipleEl.textContent.trim();
         }
 
-        let gender: string;
-        switch(notTranslatedGender) {
-            case "weiblecht": gender = "feminine"; break;
-            case "männlecht": gender = "masculine"; break;
-            default: gender = "neutral"; break;
+        const types: WordType[] = [];
+
+        for(const key of typeKeys) {
+            const typeDetails: WordTypeDetails = {};
+
+            if(key === "noun") {
+                let gender: string;
+                switch(notTranslatedGender) {
+                    case "weiblecht": gender = "feminine"; break;
+                    case "männlecht": gender = "masculine"; break;
+                    default: gender = "neutral"; break;
+                }
+                typeDetails.nounGender = gender;
+            }
+
+            if(key.endsWith("verb")) {
+                typeDetails.auxiliaryVerb = auxiliaryVerb;
+                typeDetails.pastParticiple = pastParticiple;
+            }
+
+            types.push({
+                type: key,
+                details: typeDetails,
+                meanings: []
+            });
         }
-        return {type: type, gender: gender, verbGroup: verbGroup};
+
+        return types;
     }
 }
